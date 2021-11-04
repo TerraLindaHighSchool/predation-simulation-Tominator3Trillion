1e04877781de0f30cb5f6ec35da88a4e2ebf0d86
Tominator3Trillion <89489218+Tominator3Trillion@users.noreply.github.com>
2021-11-04 15:44:42 -0700

    Averages

--- Forest.java
+++ Forest.java
@@ -2,6 +2,7 @@ import greenfoot.*;  // (Actor, World, Greenfoot, GreenfootImage)
 import java.util.List;
 import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.File;
 
 /**
  * Creates the world for predation events to occur and
@@ -19,12 +20,22 @@ public class Forest extends World
     private final int START_CARNIVORE = 3;
     private final int STEP_REPORTING = 50;
     private final int MAX_STEPS = 5000;
+    private final String ABREVIATION = "modified";
     
     // Creates fields for writing data
     private FileWriter myWriter;
     private int step;
     private Animal animal;
     private Plant plant;
+    
+    private static int iteration = 0;
+    private static int MAX_ITERATIONS = 5;
+    
+    int plants[]= new int[MAX_STEPS+5];
+    int herbivores[]= new int[MAX_STEPS+5];
+    int carnivores[] = new int[MAX_STEPS+5];
+    
+    
     
     // Forest constructor
     public Forest() 
@@ -76,7 +87,16 @@ public class Forest extends World
         
         try
         {
-            myWriter = new FileWriter("data.txt");
+            int i = 0;
+            boolean exists = true;
+            while(exists) {
+                File tempFile = new File(ABREVIATION+i+".txt");
+                exists=tempFile.exists();
+                i++;
+            }
+            
+            
+            myWriter = new FileWriter(ABREVIATION+(i-1)+".txt");
         } catch (IOException e)
         {
             System.out.println("An error occurred.");
@@ -90,13 +110,24 @@ public class Forest extends World
         List PlantObjects = getObjects(Plant.class);
         List HerbivoreObjects = getObjects(Herbivore.class);        
         List CarnivoreObjects = getObjects(Carnivore.class);
+        if(iteration==0) {
+
+            plants[step] = PlantObjects.size();
+            herbivores[step] = HerbivoreObjects.size();
+            carnivores[step] = CarnivoreObjects.size();
+        } else {
+            plants[step]  += PlantObjects.size();
+            herbivores[step] += HerbivoreObjects.size();
+            carnivores[step] += CarnivoreObjects.size();
+        }
         if(step % STEP_REPORTING == 0)
         {
+            
             String stepLine = "Step: " + step;
             String plantLine = "Plants: " + PlantObjects.size();
             String herbivoreLine = "Herbivore: " + HerbivoreObjects.size();
             String carnivoreLine = "Carnivore: " + CarnivoreObjects.size();
-            System.out.format("%15s%13s%16s%18s\n", stepLine, plantLine, herbivoreLine, carnivoreLine);
+            System.out.format("%15s%13s%16s%18s%14s\n", "Iteration: " +iteration, stepLine, plantLine, herbivoreLine, carnivoreLine);
             try
             {
                 myWriter.append(stepLine + "\t" + plantLine + "\t" + herbivoreLine + "\t" + carnivoreLine + "\n");
@@ -110,11 +141,20 @@ public class Forest extends World
     
     private void endSimulation() 
     {
-        if(step > MAX_STEPS)
-        {       
+        if(iteration >= MAX_ITERATIONS) {
             try
             {
-                myWriter.close();
+                FileWriter averageFile = new FileWriter(ABREVIATION+"-average.txt");
+                for(int i =1; i < MAX_STEPS+1; i++) {
+                    if(i%STEP_REPORTING==0) {
+                        System.out.print("Step: " + i+ "\t" + "Plants: " + plants[i]+ "\t" + "Herbivore: " + herbivores[i]+ "\t" + "Carnivore: " + carnivores[i] +"\n");
+    
+                        averageFile.append("Step: " + i+ "\t" + "Plants: " + (plants[i]/MAX_ITERATIONS)+ "\t" + "Herbivore: " + (herbivores[i]/MAX_ITERATIONS)+ "\t" + "Carnivore: " + (carnivores[i]/MAX_ITERATIONS) +"\n");
+                    }
+                }
+                
+                
+                averageFile.close();
                 System.out.println("End of simulation");
                 Greenfoot.stop();
             } catch (IOException e)
@@ -123,6 +163,25 @@ public class Forest extends World
                 e.printStackTrace();
             }
         }
+        if(step > MAX_STEPS)
+        {    
+            iteration++;
+            
+            
+            try
+            {
+                myWriter.close();
+            } catch (IOException e)
+            {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            if(!(iteration >= MAX_ITERATIONS)) {
+                Greenfoot.setWorld(new Forest());
+            }
+
+        }
+        
 
     }
 }
